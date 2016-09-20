1bb9d2727bec62490fc2c270dc1ed15c0ea399fb
sigonasr2 <sigonasr2@gmail.com>
2016-09-19 19:43:43 -0500

    Fix reverse logic on hands for Arrow Quiver swapping.

--- src/sig/plugin/TwosideKeeper/TwosideKeeper.java
+++ src/sig/plugin/TwosideKeeper/TwosideKeeper.java
@@ -3111,8 +3111,8 @@ public class TwosideKeeper extends JavaPlugin implements Listener {
 	@EventHandler(priority=EventPriority.LOW,ignoreCancelled = true)
     public void onPlayerSwapItem(PlayerSwapHandItemsEvent ev) {
 		Player p = ev.getPlayer();
-    	if (ev.getMainHandItem()!=null &&
-    			ev.getMainHandItem().getType()==Material.BOW && ArrowQuiver.isValidQuiver(ev.getMainHandItem())) {
+    	if (ev.getOffHandItem()!=null &&
+    			ev.getOffHandItem().getType()==Material.BOW && ArrowQuiver.isValidQuiver(ev.getMainHandItem())) {
     		ev.setCancelled(true);
     		//Swap forward or backward modes, depending on whether we are sneaking of not.
     		ItemStack quiver = p.getEquipment().getItemInOffHand();
