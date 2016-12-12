640ea50149c5c2cc1e87e030f639d63d7adcd7ec
sigonasr2 <sigonasr2@gmail.com>
2016-12-11 20:30:00 -0600

    Make regular item pickup sound global.

--- src/sig/plugin/TwosideKeeper/HelperStructures/Common/GenericFunctions.java
+++ src/sig/plugin/TwosideKeeper/HelperStructures/Common/GenericFunctions.java
@@ -3838,7 +3838,7 @@ public class GenericFunctions {
 	public static boolean giveItem(Player p, ItemStack... items) {
         HashMap<Integer,ItemStack> remaining = p.getInventory().addItem(items);
         if (remaining.isEmpty()) {
-        	SoundUtils.playLocalSound(p, Sound.ENTITY_ITEM_PICKUP, 1.0f, 1.0f);
+        	SoundUtils.playGlobalSound(p.getLocation(), Sound.ENTITY_ITEM_PICKUP, 1.0f, 1.0f);
             return true;
         } else {
             for (Integer i : remaining.keySet()) {
